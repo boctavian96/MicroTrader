80c3fa0e7ecca254bb18ea261e24422bad718364
boctavian96 <boctavian96@yahoo.com>
2020-02-29 13:38:39 +0200

    audit-service skeleton

---
+++ audit-service/src/main/java/com/trader/audit/App.java
@@ -0,0 +1,13 @@
+package com.trader.audit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
