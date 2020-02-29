487268cb00e6a2dd95c06ead32a1c59c72747f34
boctavian96 <boctavian96@yahoo.com>
2020-02-29 15:18:53 +0200

    Common for the microservices.

---
+++ services-common/src/main/java/com/services/utils/Launcher.java
@@ -0,0 +1,62 @@
+package com.services.utils;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.json.JsonObject;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+
+/**
+ * Hello world!
+ *
+ */
+public class Launcher extends io.vertx.core.Launcher {
+
+	private static final String CONFIG_PATH = "conf/config.json";
+	static final Logger LOGGER = LoggerFactory.getLogger(Launcher.class.getName());
+
+	public static void main(String[] args) {
+		new Launcher().dispatch(args);
+	}
+
+	@Override
+	public void beforeDeployingVerticle(DeploymentOptions deploymentOptions) {
+		super.beforeDeployingVerticle(deploymentOptions);
+
+		try {
+			readConfig(deploymentOptions);
+		} catch (FileNotFoundException e) {
+			LOGGER.error(e.getMessage(), e.getCause());
+		}
+	}
+
+	private void defaultOptions(DeploymentOptions options) {
+		if (options.getConfig() != null) {
+			LOGGER.info("No available options!");
+			return;
+		}
+
+		options.setConfig(new JsonObject());
+	}
+
+	private void readConfig(DeploymentOptions options) throws FileNotFoundException {
+		File configFile = new File(CONFIG_PATH);
+		JsonObject config = getConfig(configFile);
+		options.getConfig().mergeIn(config);
+	}
+
+	private JsonObject getConfig(File config) throws FileNotFoundException {
+		if (!config.isFile()) {
+			LOGGER.info("Config is not a file!");
+			return new JsonObject();
+		}
+
+		Scanner scanner = new Scanner(config).useDelimiter("\\A");
+		String confStr = scanner.next();
+
+		return new JsonObject(confStr);
+	}
+}
---
+++ services-common/src/main/java/com/services/utils/MicroserviceVerticle.java
@@ -0,0 +1,86 @@
+package com.services.utils;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Future;
+import io.vertx.core.Handler;
+import io.vertx.core.impl.ConcurrentHashSet;
+import io.vertx.core.json.JsonObject;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+import io.vertx.servicediscovery.Record;
+import io.vertx.servicediscovery.ServiceDiscovery;
+import io.vertx.servicediscovery.ServiceDiscoveryOptions;
+import io.vertx.servicediscovery.types.HttpEndpoint;
+
+public class MicroserviceVerticle extends AbstractVerticle {
+
+	final static Logger LOGGER = LoggerFactory.getLogger(MicroserviceVerticle.class.getName());
+
+	private ServiceDiscovery discovery;
+	private Set<Record> records = new ConcurrentHashSet<>();
+
+	private void createServiceDiscovery() {
+		JsonObject config = config();
+		ServiceDiscoveryOptions options = new ServiceDiscoveryOptions().setBackendConfiguration(config);
+		discovery = ServiceDiscovery.create(vertx, options);
+		LOGGER.info("Created the service discovery");
+	}
+
+	private void publish(Record record, Handler<AsyncResult<Void>> completion) throws Exception {
+		if (discovery == null) {
+			start();
+		}
+
+		discovery.publish(record, ar -> {
+			if (ar.succeeded()) {
+				records.add(record);
+			}
+			completion.handle(ar.map((Void) null));
+		});
+	}
+
+	private Future<Void> unpublish(Record record) {
+		Future<Void> unregisteringFuture = Future.future();
+		discovery.unpublish(record.getRegistration(), unregisteringFuture);
+
+		return unregisteringFuture;
+	}
+
+	private void stopDiscovery(Future<Void> stopFuture) {
+		discovery.close();
+		stopFuture.complete();
+		LOGGER.info("Stoped the discovery service");
+	}
+
+	protected void publishHttpEndpoint(String endpoint, String host, int port, Handler<AsyncResult<Void>> completion)
+			throws Exception {
+		Record record = HttpEndpoint.createRecord(endpoint, host, port, "/");
+		publish(record, completion);
+	}
+
+	@Override
+	public void stop(Future<Void> stopFuture) throws Exception {
+		super.stop(stopFuture);
+
+		List<Future> futures = records.stream().map(this::unpublish).collect(Collectors.toList());
+
+		if (futures.isEmpty()) {
+			stopDiscovery(stopFuture);
+		} else {
+			//stopServices(futures, stopFuture);
+		}
+	}
+
+	@Override
+	public void start() throws Exception {
+		super.start();
+
+		createServiceDiscovery();
+	}
+
+}
