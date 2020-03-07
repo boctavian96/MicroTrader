62c76260b41c3000f6031d9de2da4ed13d6c9006
boctavian96 <boctavian96@yahoo.com>
2020-03-07 23:02:17 +0200

    Audit basics.

--- audit-service/src/main/java/com/trader/audit/AuditServiceImpl.java
+++ audit-service/src/main/java/com/trader/audit/AuditServiceImpl.java
@@ -4,12 +4,36 @@ import io.vertx.core.AbstractVerticle;
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Handler;
 import io.vertx.core.json.JsonObject;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+import io.vertx.ext.web.Route;
+import io.vertx.ext.web.Router;
 
 public class AuditServiceImpl extends AbstractVerticle implements AuditService {
 
+	private static final int PORT = 9002;
+	private static final Logger LOGGER = LoggerFactory.getLogger(AuditServiceImpl.class.getName());
+
+	private Router router = Router.router(vertx);
+
+	// Routes
+	private Route checkTransaction = router.get("/api/audit/:transaction");
+
+	private void handleRoutes() {
+		checkTransaction.handler(requestHandler -> {
+			requestHandler.response().end("Alles im Ordnung :)");
+		});
+	}
+
 	@Override
 	public void auditTransaction(Handler<AsyncResult<JsonObject>> resultHandler) {
-		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void start() {
+		handleRoutes();
+		vertx.createHttpServer().requestHandler(router).listen(PORT);
 
 	}
 
