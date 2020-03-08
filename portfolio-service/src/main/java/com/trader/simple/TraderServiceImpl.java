9fcd9f7dcdd70caea0fe61700c8ddb3a7d79ab9e
boctavian96 <boctavian96@yahoo.com>
2020-03-08 16:48:10 +0200

    Trader service rest.

--- portfolio-service/src/main/java/com/trader/simple/TraderServiceImpl.java
+++ portfolio-service/src/main/java/com/trader/simple/TraderServiceImpl.java
@@ -7,6 +7,7 @@ import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
 import io.vertx.ext.web.Route;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.StaticHandler;
 
 public class TraderServiceImpl extends AbstractVerticle implements TraderService {
 
@@ -19,12 +20,46 @@ public class TraderServiceImpl extends AbstractVerticle implements TraderService
 	Route docRoute = router.get("/");
 	Route clientsRoute = router.get("/api/clients");
 	Route clientRoute = router.get("/api/:clientId");
-	Route addClient = router.put("/api/newClient/:clientId/:clientName/:clientStocks");
+	Route addClient = router.post("/api/newClient/:clientId/:clientName/:clientStocks");
+	Route buyStocks = router.put("/api/buy/:clientId/:amount");
+	Route sellStocks = router.put("/api/sell/:clientId/:amount");
 
 	private void createRoutes() {
 		docRoute.handler(requestHandler -> {
 			requestHandler.response().end("Here should be some docs...");
 		});
+		
+		clientsRoute.handler(requestHandler -> {
+			requestHandler.response().end("all the users");
+		});
+		
+		clientRoute.handler(requestHandler ->{
+			String id = requestHandler.get("clientId");
+			
+			requestHandler.response().end("Everything works " + id);
+		});
+		
+		addClient.handler(requestHandler ->{
+			String id = requestHandler.get("clientId");
+			String clientName = requestHandler.get("clientName");
+			String clientStocks = requestHandler.get("clientStocks");
+			
+			requestHandler.response().end("Works");
+		});
+		
+		buyStocks.handler(requestHandler->{
+			String id = requestHandler.get("clientId");
+			String amount = requestHandler.get("amount");
+			
+			requestHandler.response().end();
+		});
+		
+		sellStocks.handler(requestHandler ->{
+			String id = requestHandler.get("clientId");
+			String amount = requestHandler.get("amount");
+		});
+		
+		router.get().handler(StaticHandler.create());
 	}
 
 	@Override
@@ -36,6 +71,8 @@ public class TraderServiceImpl extends AbstractVerticle implements TraderService
 	@Override
 	public void sellStocks(double amount, Handler<AsyncResult<Void>> resultHandler) {
 		// TODO Auto-generated method stub
+
+		
 
 	}
 
