9d8debe868503b937a5576e4ca0248152839301f
boctavian96 <boctavian96@yahoo.com>
2020-03-08 19:50:36 +0200

    Trader server.

--- portfolio-service/src/main/java/com/trader/simple/TraderServer.java
+++ portfolio-service/src/main/java/com/trader/simple/TraderServer.java
@@ -2,9 +2,17 @@ package com.trader.simple;
 
 import io.vertx.core.Vertx;
 
-public class TraderServer{
+public class TraderServer {
 
 	public static void main(String[] args) {
+		int port;
+
+		if (args.length == 0) {
+			port = 9001;
+		} else {
+			port = Integer.valueOf(args[0]);
+		}
+
 		Vertx vertx = Vertx.vertx();
 		vertx.deployVerticle(new TraderServiceImpl());
 	}
